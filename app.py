dcac58ac1922f95bfc0099d06bba4259ebdb21d3
slimanemd <slimanemd@hotmail.com>
2021-07-22 11:24:57 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+# flask
+from flask import Flask, render_template
+
+# app
+app = Flask(__name__)
+
+# home
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+# ===========================================================================
+# webhook
+
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    import git
+    import os
+
+    repo = git.Repo('/home/slimanemd/reacts01')
+    repo.remotes.origin.pull('main')
+
+    app_loader = "/var/www/slimanemd_pythonanywhere_com_wsgi.py"
+    os.system("touch " + app_loader)
+
+    msg = "Updated site version successfully oh"
+    return msg, 200
+
+# ==================================================================================================
+# main
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=5000)
---
+++ templates/precache-manifest.49aab9bc6c9651bd0c5f1a62d877fdd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6480da8adc888389288ff06240d52c87",
+    "url": "/emoji-search/index.html"
+  },
+  {
+    "revision": "2523fcd68450d3fb0adf",
+    "url": "/emoji-search/static/css/2.90da6880.chunk.css"
+  },
+  {
+    "revision": "1e965901f1a79c79e4fc",
+    "url": "/emoji-search/static/css/main.f38963d9.chunk.css"
+  },
+  {
+    "revision": "2523fcd68450d3fb0adf",
+    "url": "/emoji-search/static/js/2.55fc3b97.chunk.js"
+  },
+  {
+    "revision": "de0a1713895ef548baca42cd10aace43",
+    "url": "/emoji-search/static/js/2.55fc3b97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e965901f1a79c79e4fc",
+    "url": "/emoji-search/static/js/main.2be3b3b8.chunk.js"
+  },
+  {
+    "revision": "d44d1531576180c634c7",
+    "url": "/emoji-search/static/js/runtime-main.d8cd5ee4.js"
+  }
+]);
