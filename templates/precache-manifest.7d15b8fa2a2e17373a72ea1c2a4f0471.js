ae5c79d0e11b78ae50ee56cf953685d8199e6a78
slimanemd <slimanemd@hotmail.com>
2021-07-22 16:50:33 -0400

    tic tac

---
+++ templates/precache-manifest.7d15b8fa2a2e17373a72ea1c2a4f0471.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66b5fb8ad7ee23b888d12b009c0c21be",
+    "url": "/react-tutorial-solutions/index.html"
+  },
+  {
+    "revision": "e08f573dab54de5b60dd",
+    "url": "/react-tutorial-solutions/static/css/main.a6b8b413.chunk.css"
+  },
+  {
+    "revision": "13c0425173494107c0fb",
+    "url": "/react-tutorial-solutions/static/js/2.a5ca6e5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial-solutions/static/js/2.a5ca6e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e08f573dab54de5b60dd",
+    "url": "/react-tutorial-solutions/static/js/main.ccdef7fd.chunk.js"
+  },
+  {
+    "revision": "7a13090e4d07042daf3b",
+    "url": "/react-tutorial-solutions/static/js/runtime-main.d04aa953.js"
+  }
+]);
--- templates/service-worker.js
+++ templates/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/counter-app/precache-manifest.0bcdf4addb110305888813b01205d461.js"
+  "/react-tutorial-solutions/precache-manifest.7d15b8fa2a2e17373a72ea1c2a4f0471.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/counter-app/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-tutorial-solutions/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
