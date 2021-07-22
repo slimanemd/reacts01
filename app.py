a4d09d203060195db7f02ace078d02f332b0db58
slimanemd <slimanemd@hotmail.com>
2021-07-22 13:43:11 -0400

    test git 4

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def webhook():
     import git
     import os
 
-    repo = git.Repo('/home/slimanemd/reacts01')
+    repo = git.Repo('/home/slimane/mysite/reacts01')
     repo.remotes.origin.pull('main')
 
     app_loader = "/var/www/slimanemd_pythonanywhere_com_wsgi.py"
