d9f910e2ce58ef3d2acd1b988de11272b63d3130
slimanemd <slimanemd@hotmail.com>
2021-07-22 16:35:58 -0400

    counter app

---
+++ templates/precache-manifest.0bcdf4addb110305888813b01205d461.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec5bd4fed912f79ab4a7",
+    "url": "/counter-app/static/css/main.c605bb5c.chunk.css"
+  },
+  {
+    "revision": "ec5bd4fed912f79ab4a7",
+    "url": "/counter-app/static/js/main.6fea9110.chunk.js"
+  },
+  {
+    "revision": "0f1f037b32656fd93884",
+    "url": "/counter-app/static/js/runtime~main.c63f0888.js"
+  },
+  {
+    "revision": "8e8f21b3bfdf04266ea2",
+    "url": "/counter-app/static/css/2.981c190a.chunk.css"
+  },
+  {
+    "revision": "8e8f21b3bfdf04266ea2",
+    "url": "/counter-app/static/js/2.cdc130cc.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/counter-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/counter-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/counter-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/counter-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/counter-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b3eb5c6e66ef38e023b6b0c389489b9c",
+    "url": "/counter-app/index.html"
+  }
+];
