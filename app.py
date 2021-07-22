db523ec7872f69ecbd46eef0598878d790f77928
slimanemd <slimanemd@hotmail.com>
2021-07-22 15:11:28 -0400

    fix issue2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 import os
 
 # app
-static_path = os.getenv('APP_STATIC_PATH') or "/static"
+static_path = "/static"  #os.getenv('APP_STATIC_PATH') or
 app = Flask(__name__ , static_url_path= static_path)
 
 # home
